998c3d10d2cec6ecc727f726adc2f0347a5cd76f
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-03-10 09:11:20 -0400

    fix: requirements

--- clean.sh
+++ clean.sh
@@ -8,6 +8,6 @@ isort src/
 pip freeze >requirements.txt
 
 # Append Python version requirement (if not already present)
-if ! grep -q "^python>=" requirements.txt; then
-    echo "python>=3.13.1" >>requirements.txt
-fi
+# if ! grep -q "^python>=" requirements.txt; then
+#     echo "python>=3.13.1" >>requirements.txt
+# fi
